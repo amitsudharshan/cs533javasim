462c449322266e646c2d7684b87b15cba7f87bba
vivek112 <vivek112@9020f03a-2c63-11de-a28d-194e1977add9>
2009-04-19 20:33:46 +0000

    vivek's pseudo code commit for his event driven model... This commit breaks the build

---
+++ trunk/src/org/cs533/processor/Processor.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.cs533.processor;
+
+import hybridcachecoherence.*;
+
+/**
+ *
+ * @author Vivek
+ */
+public class Processor
+{
+    private Cache cache;
+    private MessageQueue outgoingMessages;
+    private MessageQueue incomingMessages;
+
+
+    public Processor()
+    {
+
+    }
+
+
+}
