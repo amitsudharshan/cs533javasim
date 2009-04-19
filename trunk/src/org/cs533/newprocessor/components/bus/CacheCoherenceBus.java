929c77a8e86a0aa812405490fc7af406bc7502f0
amit.sudharshan <amit.sudharshan@9020f03a-2c63-11de-a28d-194e1977add9>
2009-04-19 21:11:16 +0000

    Updated

--- trunk/src/org/cs533/newprocessor/components/bus/CacheCoherenceBus.java
+++ trunk/src/org/cs533/newprocessor/components/bus/CacheCoherenceBus.java
@@ -2,13 +2,38 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.cs533.newprocessor.components.bus;
+
+import java.util.ArrayList;
+import org.cs533.newprocessor.ComponentInterface;
+import org.cs533.newprocessor.components.memorysubsystem.L1Cache;
+import org.cs533.newprocessor.components.memorysubsystem.L2Cache;
 
 /**
  *
  * @author amit
  */
-public class CacheCoherenceBus {
+public class CacheCoherenceBus implements ComponentInterface {
 
+    ArrayList<L1Cache> l1Caches;
+    L2Cache l2Cache;
+    public static final int LATENCY = 25; //FROM Veenstra/Fowler May 1992
+
+    public void registerCache(L1Cache l1Cache) {
+        if (!l1Caches.contains(l1Cache)) {
+            l1Caches.add(l1Cache);
+        }
+    }
+
+    public void runPrep() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void runClock() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int getLatency() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
--- trunk/src/org/cs533/newprocessor/components/memorysubsystem/L1Cache.java
+++ trunk/src/org/cs533/newprocessor/components/memorysubsystem/L1Cache.java
@@ -2,13 +2,30 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.cs533.newprocessor.components.memorysubsystem;
+
+import org.cs533.newprocessor.ComponentInterface;
+import org.cs533.newprocessor.components.bus.CacheCoherenceBus;
 
 /**
  *
  * @author amit
  */
-public class L1Cache {
+public class L1Cache implements ComponentInterface {
 
+    public L1Cache(CacheCoherenceBus bus) {
+        bus.registerCache(this);
+    }
+
+    public void runPrep() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void runClock() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int getLatency() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
