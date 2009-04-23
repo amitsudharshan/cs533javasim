fddd4d111e535aa96b8efb4742e6d9faf3980c92
vivek112 <vivek112@9020f03a-2c63-11de-a28d-194e1977add9>
2009-04-23 03:17:32 +0000

    test for L1MESI.java.   This is where I'm getting NPE at runtime.  Not sure how to use LRUEvictHashMap, but hopefully you can see how this will work with other components.

--- trunk/src/org/cs533/newprocessor/components/memorysubsystem/L1_MESI_Test.java
+++ trunk/src/org/cs533/newprocessor/components/memorysubsystem/L1_MESI_Test.java
@@ -8,77 +8,88 @@ package org.cs533.newprocessor.components.memorysubsystem;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
-//import org.cs533.newprocessor.components.memorysubsystem.L1MESI.Event;
+import org.cs533.newprocessor.components.memorysubsystem.L1MESICacheLine;
+import org.cs533.newprocessor.components.memorysubsystem.L1MESICacheLine.Event;
 /**
  *
  * @author Vivek
  */
+
 public  class L1_MESI_Test
 {
-//
-//    L1Cache myL1Cache = new L1Cache(, );
-//    Random rand = new Random();
-//    LRUEvictHashTable<CacheLine> L1Lines =  new LRUEvictHashTable(10);
-//
-//  public L1_MESI_Test(int data)
-//  {
-//      L1MESI newCacheLine =  null;
-//    for (int i = 0; i<10; i++)
-//    {
-//     newCacheLine = new L1MESI((rand.nextInt()%20), false) ;
-//    L1Lines.add(newCacheLine);
-//    }
-//      if(newCacheLine != null)
-//         System.out.println(newCacheLine.toString());
-//
-//
-//
-//  //do many differnt instructions at this...\
-//      for (int i = 0; i<10; i++)
-//    {
-//     newCacheLine = new L1MESI((rand.nextInt()%90), false) ;
-//    L1Lines.add(newCacheLine);
-//    if(newCacheLine != null)
-//         System.out.println(newCacheLine.toString());
-//    }
-//
-//      Iterator iter = L1Lines.values().iterator();
-//      while(iter.hasNext())
-//      {
-//        L1MESI x = (L1MESI) L1Lines.values().iterator().next();
-//        x.onMessage(2, Event.BusRead, 0, false, data);
-//        x.onMessage(3, Event.PWrite, 0, false, data);
-//        x.onMessage(1, Event.PWrite, 0, false, data);
-//        x.onMessage(1, Event.PWrite, 0, false, data);
-//        x.onMessage(1, Event.PWrite, 0, false, data);
-//        x.onMessage(1, Event.PWrite, 0, false, data);
-//        x.onMessage(1, Event.PWrite, 0, false, data);
-//        x.onMessage(1, Event.BusRead, 0, false, data);
-//        x.onMessage(3, Event.PWrite, 0, false, data);
-//        x.onMessage(3, Event.BusInvalidate, 0, false, data);
-//        x.onMessage(4, Event.BusInvalidate, 0, false, data);
-//
-//      }
-//
-//      Iterator iter2 = L1Lines.values().iterator();
-//     L1MESI y = (L1MESI) iter2.next();
-//     int counter = 1;
-//      while(iter.hasNext())
-//      {
-//         y = (L1MESI) iter2.next();
-//        System.out.println("The "+ counter +  " th iteration gives" + y.toString());
-//
-//
-//      }
-//  }
-//
-//
-//  public static void main(String[] args)
-//  {
-//      System.out.println("beginning test for L1_MESI_Test");
-//      L1_MESI_Test testL1 = new L1_MESI_Test(17);
-//
-//      System.out.println("completed test");
-//  }
-}
+    L1MESICacheLine myL1Cache = new L1MESICacheLine();
+    Random rand = new Random();
+    LRUEvictHashTable<CacheLine> L1Lines =  new LRUEvictHashTable(10);
 
+  public L1_MESI_Test(int data)
+  {
+    L1MESICacheLine newCacheLine =  null;
+    for (int i = 0; i<10; i++)
+    {
+     newCacheLine = new L1MESICacheLine((rand.nextInt()%20), false);
+    L1Lines.add(newCacheLine);
+    }
+      if(newCacheLine != null)
+         System.out.println(newCacheLine.toString());
+
+
+
+  //do many differnt instructions at this...\
+      for (int i = 0; i<10; i++)
+    {
+     System.out.println("creating new cache lines");
+     newCacheLine = new L1MESICacheLine((rand.nextInt()%90), false) ;
+    L1Lines.add(newCacheLine);
+    if(newCacheLine != null)
+    {
+       
+         System.out.println(newCacheLine.toString());
+    }
+    else
+     {
+      System.out.println("cache line was null!");
+     }
+    }
+
+     System.out.println("created all the cache lines successfully");
+ 
+
+    
+     L1MESICacheLine x = null;
+     L1MESICacheLine[] myLines =  (L1MESICacheLine[]) L1Lines.values().toArray();
+     Iterator iter = L1Lines.values().iterator();
+     System.out.println("beginning simulated events on state machine");
+     int i = 0;
+   
+         for(i = 0; i< myLines.length; i++)
+         {
+         System.out.println("Simulation: On the iteration " + i  );
+        //models a producer-consumer pattern
+
+        myLines[i].onMessage(2, Event.BusRead, 0, false, data);
+        myLines[i].onMessage(3, Event.PWrite, 0, true, data);
+        myLines[i].onMessage(1, Event.PWrite, 0, false, data);
+        myLines[i].onMessage(1, Event.PWrite, 0, false, data);
+        myLines[i].onMessage(1, Event.PWrite, 0, false, data);
+        myLines[i].onMessage(1, Event.PWrite, 0, false, data);
+        myLines[i].onMessage(1, Event.PWrite, 0, false, data);
+        myLines[i].onMessage(1, Event.BusRead, 0, false, data);
+        myLines[i].onMessage(3, Event.PWrite, 0, true, data);
+        myLines[i].onMessage(3, Event.BusInvalidate, 0, true, data);
+        myLines[i].onMessage(4, Event.BusInvalidate, 0, false, data);
+        System.out.println("ending iteration"+ i);
+         }
+     for(i = 0; i< myLines.length; i++)
+      {  
+        System.out.println("The "+ i +  " th iteration gives" + myLines[i].toString());
+      }
+  }
+
+
+  public static void main(String[] args)
+  {
+      System.out.println("beginning test for L1_MESI_Test");
+      L1_MESI_Test testL1 = new L1_MESI_Test(17);
+      System.out.println("completed test");
+  }
+ }
