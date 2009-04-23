345d23465b119aaf974b5f09b50bc238720994f8
vivek112 <vivek112@9020f03a-2c63-11de-a28d-194e1977add9>
2009-04-23 00:55:22 +0000

    this is the superclass implementing a standard cacheline

--- trunk/src/org/cs533/newprocessor/components/memorysubsystem/CacheLine.java
+++ trunk/src/org/cs533/newprocessor/components/memorysubsystem/CacheLine.java
@@ -12,12 +12,19 @@ package org.cs533.newprocessor.components.memorysubsystem;
 public class CacheLine {
     public int data;
     public boolean shared;
-
+    public int state;
 
     public CacheLine(int _data, boolean _shared)
     {
         data = _data;
+        this.state = 2;//assume newly arriving data into cache is brought in from
+                       //memory.  Thus, we are always at valid-exclusive
         this.shared = _shared;
     }
-
+    public CacheLine( int _data, int _state)
+    {
+        this.data = _data;
+        this.state = _state;
+    }
 }
+
