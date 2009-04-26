f78efc33bd0e4366cfb0d2400a782131b466d723
brandon.cs533 <brandon.cs533@9020f03a-2c63-11de-a28d-194e1977add9>
2009-04-26 02:32:55 +0000

    Generic Protocol system

--- trunk/src/org/cs533/newprocessor/components/memorysubsystem/L1Cache.java
+++ trunk/src/org/cs533/newprocessor/components/memorysubsystem/L1Cache.java
@@ -4,38 +4,67 @@
  */
 package org.cs533.newprocessor.components.memorysubsystem;
 
+import java.util.ArrayList;
+
 import org.cs533.newprocessor.ComponentInterface;
+import org.cs533.newprocessor.components.bus.BusAggregator;
+import org.cs533.newprocessor.components.bus.BusClient;
 import org.cs533.newprocessor.components.bus.CacheCoherenceBus;
+import org.cs533.newprocessor.components.bus.CoherenceProtocol;
+import org.cs533.newprocessor.components.bus.ProtocolContext;
 import org.cs533.newprocessor.components.core.ProcessorCore;
 
 /**
  *
  *
  */
-public class L1Cache implements ComponentInterface {
+public class L1Cache<BusMessage, LineStates,
+        Protocol extends CoherenceProtocol<BusMessage,LineStates>>
+        implements ComponentInterface, BusClient<BusMessage>, MemoryInterface,
+        ProtocolContext<LineStates> {
 
     ProcessorCore core;
-    CacheCoherenceBus bus;
+    CacheCoherenceBus<BusMessage> bus;
+    ArrayList<MemoryInstruction> pendingRequests;
+    Protocol proto;
     public static final int LATENCY = 1;
 
+    public L1Cache (Protocol proto) {
+        this.proto = proto;
+        proto.setContext(this);
+    }
 
-    public L1Cache(ProcessorCore _core, CacheCoherenceBus _bus) {
+    public MemoryInstruction getNextRequest() {
+        return pendingRequests.remove(0);
+    }
+    LRUEvictHashTable<CacheLine<LineStates>> data;
+    public LRUEvictHashTable<CacheLine<LineStates>> getData() {return data;}
+
+    public void enqueueMemoryInstruction(MemoryInstruction request)
+    {
+        pendingRequests.add(request);
+    }
+
+    public L1Cache(ProcessorCore _core, CacheCoherenceBus _bus, Protocol _proto) {
         bus = _bus;
         core = _core;
-       //
+        proto = _proto;
     }
 
-    L1Cache() {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
+    public void recieveMessage(BusMessage msg) { proto.recieveMessage(msg);}
+    public BusMessage getResponse() {return proto.getResponse();}
+    public BusMessage getBusMessage() {return proto.getBusMessage();}
+    public BusAggregator<BusMessage> getAggregator() {return proto.getAggregator();}
+    public MemoryInstruction getMemoryRequest() {return proto.getMemoryRequest();}
+    public void recieveMemoryResponse(MemoryInstruction resp) { proto.recieveMemoryResponse(resp);}
+    
     public void runPrep()
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        proto.runPrep();
     }
 
     public void runClock() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        proto.runClock();
     }
 
     public int getLatency() {
