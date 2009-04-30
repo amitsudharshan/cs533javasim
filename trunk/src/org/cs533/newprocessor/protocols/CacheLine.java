8ef3435616c705a25ca311eb47a8706ca1acfa22
vivek112 <vivek112@9020f03a-2c63-11de-a28d-194e1977add9>
2009-04-30 07:18:08 +0000

    This is the folder with the protocols, in addition to the test driver that I created earlier last night. Run Main.java to use the driver to check.   Also, remember to change the path to the test files, as it is currently hardcoded, and there is no way to do relative paths.

---
+++ trunk/src/org/cs533/newprocessor/protocols/CacheLine.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.cs533.newprocessor.protocols;
+
+import CacheCoherenceProtocols.Main.EventType;
+
+/**
+ *
+ * @author Vivek
+ */
+public  class CacheLine
+{
+    //standard transitions in most all cache coherence protocols
+
+    public enum Transition
+    {
+        pRead, pWrite, busRead, busWrite, pWriteMissServed, busInvalidate, ackInvalidate, busUpdate, updateAck
+    }
+  
+
+    protected int address;
+    protected int processor;
+
+    //constructor
+    public CacheLine(int _address, int _processor, boolean shared)
+    {
+        address = _address;
+        processor = _processor;
+    }
+
+    public void processEvent(int EventType)
+    {
+
+          //not to be implemented
+        // the subclass implements this
+    }
+   void processBusEvent(EventType eventType) {
+
+         //not to be implemented
+        // the subclass implements this
+    }
+
+    public void print()
+    {
+         System.out.println(Main.currentTime + " :  state of Cache Line " + address + " is "   );
+    }
+
+
+
+}
