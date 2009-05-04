fd4fde2f0cc6df30a0f779e628c8332ddacebae6
brandon.cs533 <brandon.cs533@9020f03a-2c63-11de-a28d-194e1977add9>
2009-05-04 21:09:12 +0000

    Better comments describing when and how methods of these interfaces will be used

--- src/org/cs533/newprocessor/components/memorysubsystem/MemoryInterface.java
+++ src/org/cs533/newprocessor/components/memorysubsystem/MemoryInterface.java
@@ -10,6 +10,7 @@ package org.cs533.newprocessor.components.memorysubsystem;
  * @author amit
  */
 public interface MemoryInterface {
-
+    // This method may only be invoked once per runPrep cycle,
+    // to ensure requests arrive in a deterministic order.
     public void enqueueMemoryInstruction(MemoryInstruction instr);
 }
