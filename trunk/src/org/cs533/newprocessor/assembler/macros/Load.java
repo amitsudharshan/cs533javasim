7a9784b3dd38dadaa1450a8aa599e1268f853559
amit.sudharshan <amit.sudharshan@9020f03a-2c63-11de-a28d-194e1977add9>
2009-05-06 23:45:37 +0000

    Adding macros capability which allow us to exapand instructions into more complex assembly

---
+++ trunk/src/org/cs533/newprocessor/assembler/macros/Load.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cs533.newprocessor.assembler.macros;
+
+/**
+ *
+ * @author amit
+ */
+public class Load extends AbstractMacro {
+
+    static final String macroExtends = "load";
+
+    public String[] runMacro(String instruction) {
+        String[] split = instruction.split(" ");
+        String[] toReturn = new String[2];
+        String register = split[1];
+        String variable = split[2];
+
+        toReturn[0] = "lui " + split[1] + " U" + split[2];
+        toReturn[1] = "ori " + split[1] + " " + split[1] +" L" + split[2];
+        return toReturn;
+    }
+
+    public String getInstructionThisMacroExtends() {
+        return macroExtends;
+    }
+}
